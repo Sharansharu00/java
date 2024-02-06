a6c14a4c9235da9f02953b594637e24a396708d0
Sharansharu00 <sharan2252001@gmail.com>
2024-02-06 12:16:29 +0530

    this code is update

--- ex1.java
+++ ex1.java
@@ -1,5 +1,5 @@
 public class ex1 {
     public static void main(String[]args){
-        System.out.println("hiii iam sharan.........");
+        System.out.println("hiii iam sharan.... this code is update.....");
     }
 }
